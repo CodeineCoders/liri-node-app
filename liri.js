1e542dee598b12c98aff3d01ae2bf443dc4171a9
Brian Racek <rac3k@aol.com>
2018-07-29 22:52:52 -0400

    research on arguments

--- liri.js
+++ liri.js
@@ -2,8 +2,8 @@ require("dotenv").config();
 var request = require("request");
 var keys = require("./keys");
 var fs = require("fs");
+
 var Twitter = require("twitter");
+var client = new Twitter(keys.twitter);
 
-var twitter = new Twitter(keys.twitter);
-
-var argument = process.argv[2];
+var argument = process.argv;
