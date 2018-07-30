a37236aba5b0cedaa48b30ca82ad976430fea346
Brian Racek <rac3k@aol.com>
2018-07-29 20:31:14 -0400

    update node

--- liri.js
+++ liri.js
@@ -1,7 +1,13 @@
 require("dotenv").config();
 var keys = require("./keys");
+var fs = require("fs");
 
-var spotify = new Spotify(keys.spotify);
-var client = new Twitter(keys.twitter);
+var spotify = require("spotify");
+var client = require("twitter");
 
+console.log("tweets, spotify this song");
 
+var userInput = process.argv[2];
+var userInput2 = process.argv[3];
+
+ 
